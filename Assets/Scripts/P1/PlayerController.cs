62e96a70a83abe52fbb42d64645b151afa49a9c5
Vladimirx900 <vladimir.quispe@davinci.edu.ar>
2024-04-23 01:38:36 -0300

    Camara v2 (sin rotacion z)

--- Assets/Scripts/P1/PlayerController.cs
+++ Assets/Scripts/P1/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     public float throttleIncrement = 0.1f;
     public float maxThrust = 180f;
     public float responsiveness = 120f;
+    private float _responseModifier = 1f;
 
     public float maxRot = 45f;
 
@@ -27,6 +28,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float _controlSnap = 1;
 
+    private bool _lockInputs = false;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -36,29 +39,33 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (_lockInputs) return;
         HandleInputs();
     }
 
     private void FixedUpdate()
     {
+        if (_lockInputs) return;
         _agent.AddForce(transform.forward * maxThrust * _throttle);
-        _yaw += _horizontalInput * responsiveness * Time.fixedDeltaTime;
-        _pitch += _verticalInput * responsiveness * Time.fixedDeltaTime;
-
+        _yaw += _horizontalInput * responsiveness * _responseModifier * Time.fixedDeltaTime;
+        _pitch += _verticalInput * responsiveness * _responseModifier * Time.fixedDeltaTime;
         _roll = Mathf.Lerp(0, 30, Mathf.Abs(_horizontalInput)) * -Mathf.Sign(_horizontalInput);
         transform.localRotation = Quaternion.Euler(Vector3.up * _yaw + Vector3.right * _pitch + Vector3.forward * _roll);
-        //_yaw = Mathf.Lerp(_yaw, 0, Time.fixedDeltaTime);
-        //_pitch = Mathf.Lerp(_pitch, 0, Time.fixedDeltaTime);
     }
 
     private void HandleInputs()
     {
-        Debug.Log("Throttle Up" + _throttleUp);
-        Debug.Log("Throttle Down" + _throttleDown);
         if (_throttleUp) _throttle += throttleIncrement;
-        if (!_throttleUp) _throttle -= throttleIncrement;
+        if (!_throttleUp && _throttle > 50f && !_throttleDown) _throttle -= throttleIncrement;
         if (_throttleDown) _throttle -= throttleIncrement * 2;
-        _throttle = Mathf.Clamp(_throttle, 0f, 100f);
+        if(_throttle >= 50f)
+        {
+            _throttle = Mathf.Clamp(_throttle, 50f, 100f);
+        }
+        else
+        {
+            _throttle = Mathf.Clamp(_throttle, 0f, 100f);
+        }
     }
 
     public void OnRoll(InputAction.CallbackContext context)
@@ -78,9 +85,9 @@ public class PlayerController : MonoBehaviour
         {
             dir.y = Mathf.Clamp(dir.y, -1, 1);
         }
+        // Prevents inputs from snapping.
         _horizontalInput = Mathf.Lerp(_horizontalInput, dir.x, _controlSnap * Time.deltaTime);
         _verticalInput = Mathf.Lerp(_verticalInput, -dir.y, _controlSnap * Time.deltaTime);
-        //_roll = dir.x;
     }
 
     public void OnThrottleUp(InputAction.CallbackContext context)
@@ -103,5 +110,26 @@ public class PlayerController : MonoBehaviour
         {
             _throttleDown = false;
         }
+    }
+
+    public void OnLockInputs(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _lockInputs = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (context.canceled)
+        {
+            _lockInputs = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void OnResponseChange(float value)
+    {
+        _responseModifier = value;
     }
 }
