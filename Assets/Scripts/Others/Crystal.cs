08df51d270345e0f843701365508863592ffc088
AidanCV <114941269+AidanCV@users.noreply.github.com>
2024-06-08 13:16:23 -0300

    particulas cristales

--- Assets/Scripts/Others/Crystal.cs
+++ Assets/Scripts/Others/Crystal.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Crystal : MonoBehaviour
 {
+
+    public GameObject Particles;
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<PlayerModel>() != null)
         {
             GameManager.Instance.Crystals++;
+            Instantiate(Particles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
