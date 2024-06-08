77a1e2b3be279f031d8d62fe45ad94cd18afcbc7
AidanCV <114941269+AidanCV@users.noreply.github.com>
2024-06-08 14:21:06 -0300

    mas feedback cristales

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    private AudioSource Audio;
+
+
+    private void Awake()
+    {
+        Audio = GetComponent<AudioSource>();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    public void PlaySound(AudioClip Sound)
+    {
+        Audio.PlayOneShot(Sound);
+    }
+}
--- Assets/Scripts/Others/Crystal.cs
+++ Assets/Scripts/Others/Crystal.cs
@@ -6,11 +6,13 @@ public class Crystal : MonoBehaviour
 {
 
     public GameObject Particles;
+    [SerializeField] AudioClip PickupSound;
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<PlayerModel>() != null)
         {
             GameManager.Instance.Crystals++;
+            AudioManager.instance.PlaySound(PickupSound);
             Instantiate(Particles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
